5caf6e7abc5521ef7241c081a528f84795732c2a
SnoopBogg <bsorbun@gmail.com>
2018-12-26 15:46:15 -0600

    still figuring things out.  We added an image view to display a card.

--- app/src/main/java/com/example/bsorbun/myapplication/MainActivity.java
+++ app/src/main/java/com/example/bsorbun/myapplication/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.bsorbun.myapplication;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,11 +17,15 @@ public class MainActivity extends AppCompatActivity {
 
         final TextView textView=(TextView)findViewById(R.id.CardName);
         final Button PickCard =  (Button)findViewById(R.id.CardPicker);
+        final ImageView card = findViewById(R.id.ShowImage);
 
         PickCard.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 String[] CardsValue = shuffler.shuffledDeck(1);
                 textView.setText(CardsValue[0]);
+                card.setImageResource(R.drawable.king_of_hearts);
             }         });
     }
+
+
 }
