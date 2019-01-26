a15520cba8a18cd75f609183cf6e65fbd0814c0b
SnoopBogg <bsorbun@gmail.com>
2019-01-26 12:58:28 -0600

    added a fragment

--- app/src/main/java/com/example/bsorbun/myapplication/CardSimulator.java
+++ app/src/main/java/com/example/bsorbun/myapplication/CardSimulator.java
@@ -1,18 +1,30 @@
 package com.example.bsorbun.myapplication;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+//import android.support.v4.app.FragmentActivity;
 
-public class CardSimulator extends AppCompatActivity {
-    Button button= (Button) findViewById(R.id.HitMe);
+
+public class CardSimulator extends AppCompatActivity implements show_text.OnFragmentInteractionListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_simulator);
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
             getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                     WindowManager.LayoutParams.FLAG_FULLSCREEN);
@@ -21,27 +33,35 @@ public class CardSimulator extends AppCompatActivity {
             int UI_OPTIONS = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
             getWindow().getDecorView().setSystemUiVisibility(UI_OPTIONS);
         }
-        setContentView(R.layout.activity_card_simulator);
 
-        button.setOnClickListener(new inner(this));
+        Button mybtn = findViewById(R.id.HitMe);
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//        show_text ExampleFragment = new show_text();
+//        fragmentTransaction.add(R.id.display_text, ExampleFragment);
+//        fragmentTransaction.commit();
+        mybtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Perform action on click
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                show_text ExampleFragment = new show_text();
+                fragmentTransaction.add(R.id.fragment_text, ExampleFragment);
+                fragmentTransaction.commit();
+            }
+        });
+    }
 
-//        final Button PickCard =  (Button)findViewById(R.id.CardPicker);
-//        final ImageView Card1 = findViewById(R.id.Card1);
-//        final ImageView Card2 = findViewById(R.id.Card2);
-//
-//        PickCard.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v) {
-//                String[] CardsValue = shuffler.shuffledDeck(1);
-//                int CardName = cardImage.cardImage(CardsValue[0]);
-//                Card1.setImageResource(CardName);
-//                CardName = cardImage.cardImage(CardsValue[1]);
-//                Card2.setImageResource(CardName);
-//            }        });
-        }
-    private class inner extends ImageHider.TestButton {
-        public inner(CardSimulator mainActivity)
-        {
-            super(mainActivity);
-        }
-    };
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+//    private class inner extends ImageHider{
+//        public inner(CardSimulator CardSimulator)
+//        {
+//            super(CardSimulator);
+//        }
+//    };
 }
+
--- app/src/main/java/com/example/bsorbun/myapplication/ImageHider.java
+++ app/src/main/java/com/example/bsorbun/myapplication/ImageHider.java
@@ -1,182 +1,41 @@
 package com.example.bsorbun.myapplication;
 
+import android.app.Activity;
 import android.content.Context;
+import android.os.Bundle;
 import android.util.AttributeSet;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
 import java.util.Scanner;
 import java.util.Stack;
+//
+public abstract class ImageHider implements View.OnClickListener {
+    private final CardSimulator main;
 
-public class ImageHider {
+    public ImageHider(CardSimulator CardSimulator)
+    {
+        this.main = CardSimulator;
+    }
+    @Override
+    public void onClick(View v) {
+//        ImageView Card1 = v.findViewById(R.id.Card1);
+//        ImageView Card2 = v.findViewById(R.id.Card2);
+//        EditText text1 = v.findViewById(R.id.textView2);
 
-    /*
-    This class is used to reduce the lines of code in the final product
-     */
-    public static class TestButton implements View.OnClickListener {
-
-        private final CardSimulator main;
-
-        public TestButton(CardSimulator mainActivity)
-        {
-            this.main = mainActivity;
-        }
-
-        @Override
-        public void onClick(View v) {
-            final ImageView Card1 = v.findViewById(R.id.Card1);
-            final ImageView Card2 = v.findViewById(R.id.Card2);
-
-            String[] CardsValue = shuffler.shuffledDeck(1);
-            int CardName = cardImage.cardImage(CardsValue[0]);
-            Card1.setImageResource(CardName);
-            CardName = cardImage.cardImage(CardsValue[1]);
-            Card2.setImageResource(CardName);
-        };
-//            final LinearLayout Layout1 = findViewById(R.id.linearLayout);
-//            final ImageView Card1 = findViewById(R.id.Card1);
-//            final LinearLayout Layout2 = findViewById(R.id.linearLayout2);
-//            final ImageView Card2 = findViewById(R.id.Card2);
-//            final LinearLayout Layout3 = findViewById(R.id.linearLayout3);
-//            final ImageView Card3 = findViewById(R.id.Card3);
-//            final LinearLayout Layout4 = findViewById(R.id.linearLayout4);
-//            final ImageView Card4 = findViewById(R.id.Card4);
-//            final LinearLayout Layout5 = findViewById(R.id.linearLayout5);
-//            final ImageView Card5 = findViewById(R.id.Card5);
-//            final LinearLayout Layout6 = findViewById(R.id.linearLayout6);
-//            final ImageView Card6 = findViewById(R.id.Card6);
-//            final LinearLayout Layout7 = findViewById(R.id.linearLayout7);
-//            final ImageView Card7 = findViewById(R.id.Card7);
-//            final LinearLayout Layout8 = findViewById(R.id.linearLayout8);
-//            final ImageView Card8 = findViewById(R.id.Card8);
-//            final LinearLayout Layout9 = findViewById(R.id.linearLayout9);
-//            final ImageView Card9 = findViewById(R.id.Card9);
-//            final LinearLayout Layout10 = findViewById(R.id.linearLayout10);
-//            final ImageView Card10 = findViewById(R.id.Card10);
-//            final LinearLayout Layout11 = findViewById(R.id.linearLayout11);
-//            final ImageView Card11 = findViewById(R.id.Card11);
-//
-//            int CardValue = 0;
-//            int HighAce = 11;
-//            int LowAce = 1;
-////            String[] Hand = new String[52 * number];
-//            String[] Hand = new String[52];
-//
-//            Stack StackedDeck = new Stack();
-////            String[] Deck = shuffler.shuffledDeck(number);
-//            String[] Deck = shuffler.shuffledDeck(1);
-//
-//
-//            for(String str : Deck){
-//                StackedDeck.push(str);
-//            }
-//            /*
-//            Not sure if this while loop works on the onclick view... I will program something out
-//            but eventually fix the problem
-//             */
-//            while (callOnClick()) {
-//                while (!StackedDeck.isEmpty()){
-//                    int Total = 0;
-//
-//                    while (Total <= 21) {
-//
-//                        Hand[0] = StackedDeck.pop().toString();
-//                        Layout1.setVisibility(View.VISIBLE);
-//                        Card1.setImageResource(cardImage.cardImage(Hand[0]));
-//                        CardValue = cards.cardValue(Hand[0]);
-//                        Total = Total + CardValue;
-//
-//                        Hand[1] = StackedDeck.pop().toString();
-//                        Layout2.setVisibility(View.VISIBLE);
-//                        Card2.setImageResource(cardImage.cardImage(Hand[0]));
-//                        CardValue = cards.cardValue(Hand[1]);
-//                        Total = Total + CardValue;
-//
-//                        if (Hand[0].contains("A") && !Hand[1].contains("A")) {
-//                            if (Total == 10) {
-//                                Total = Total + HighAce;
-//                                break;
-//                            } else {
-//                                Total = Total + HighAce;
-//                            }
-//                        }
-//                        if (Hand[1].contains("A") && !Hand[0].contains("A")) {
-//                            if (Total == 10) {
-//                                Total = Total + HighAce;
-//                                break;
-//                            } else {
-//                                Total = Total + HighAce;
-//                            }
-//                        }
-//                        if (Hand[0].contains("A") && Hand[1].contains("A")) {
-//                            Total = HighAce + LowAce;
-//                        }
-//
-//                        Context context = context.getApplicationContext();
-//                        CharSequence text = "Hit you?!";
-//                        int duration = Toast.LENGTH_SHORT;
-//
-//                        Toast toast = Toast.makeText(context, text, duration);
-//                        toast.show();
-//
-//
-//                        while (callOnClick()) {
-//
-//                            Hand[2] = StackedDeck.pop().toString();
-//                            Layout2.setVisibility(View.VISIBLE);
-//                            Card3.setImageResource(cardImage.cardImage(Hand[2]));
-//                            CardValue = cards.cardValue(Hand[2]);
-//
-//                            if (Hand[2].contains("A")) {
-//                                Total = Total + HighAce;
-//                                if (Total > 21) {
-//                                    Total = Total - HighAce;
-//                                    Total = Total - LowAce;
-//                                    if (Total > 21) {
-//                                        Total = 0;
-//                                        break;
-//                                    }
-//                                    if (Total == 21) {
-//                                        Total = 0;
-//                                        break;
-//                                    }
-//                                } else {
-//                                    Total = Total;
-//                                }
-//                            }
-//                            Total = Total + CardValue;
-//                            if (Total == 21) {
-//                                Total = 0;
-//                                break;
-//                            }
-//                            if (Total > 21) {
-//                                Total = 0;
-//                                break;
-//                            }
-//                            else {
-//                                toast.show();
-//                            }
-//                        }
-//                        break;
-//                    }
-///*				if (StackedDeck.isEmpty()){
-//					System.out.println("We ran out, do you want more cards?");
-//					Scanner answer = new Scanner(System.in);
-//					int answers = answer.nextInt();
-//					if (answers == 1){
-//						for(String str : Deck){
-//							StackedDeck.push(str);
-//						}
-//					}
-//				} else {
-//					break;
-//				}*/
-//                }
-//            }
-        }
+//        String[] CardsValue = shuffler.shuffledDeck(1);
+//        int CardName = cardImage.cardImage(CardsValue[0]);
+//        text1.setText("Hello there");
+//        Card1.setImageResource(CardName);
+//        CardName = cardImage.cardImage(CardsValue[1]);
+//        Card2.setImageResource(CardName);
 
     }
+
+}
